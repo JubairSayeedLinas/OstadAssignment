2b69098dd908554efedf83c12270e3bdf5552963
Jubair Sayeed Linas <jubair.sayeed@gmail.com>
2023-06-12 16:23:52 +0600

    Create assignment6.dart
    Ostad Assignment 6

---
+++ assignment6.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final List<String> photoUrls = [
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(fontSize: 24.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search photos',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(8.0),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            Wrap(
+              runSpacing: 10.0, // Spacing between rows
+              children: List.generate(
+                photoUrls.length,
+                    (index) => Container(
+                  width: MediaQuery.of(context).size.width / 3, // Adjust width based on the number of images per row
+                  child: Stack(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          final snackBar = SnackBar(content: Text('Clicked on photo!'));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            photoUrls[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            color: Colors.black54,
+                            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                            child: Text(
+                              'Image $index',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+
+            ListView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                ListTile(
+                  leading: Icon(Icons.photo),
+                  title: Text('Sample Photo 1'),
+                  subtitle: Text('Description for Sample Photo 1'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo),
+                  title: Text('Sample Photo 2'),
+                  subtitle: Text('Description for Sample Photo 2'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo),
+                  title: Text('Sample Photo 3'),
+                  subtitle: Text('Description for Sample Photo 3'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final snackBar =
+          SnackBar(content: Text('Photos Uploaded Successfully!'));
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Icon(Icons.cloud_upload),
+      ),
+    );
+  }
+}
