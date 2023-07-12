2aa6106762fe7a70c29054ea2e6549f022bd6f91
Jubair Sayeed Linas <jubair.sayeed@gmail.com>
2023-07-12 23:50:58 +0600

    Update LocationText.dart

--- Assignment9/LocationText.dart
+++ Assignment9/LocationText.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LocationText extends StatelessWidget {
+  const LocationText({
+    super.key,
+    required this.text,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.displaySmall!.copyWith(
+          color: Colors.white,
+          fontFamily: 'LilitaOne',
+          fontWeight: FontWeight.bold),
+    );
+  }
+}
 
